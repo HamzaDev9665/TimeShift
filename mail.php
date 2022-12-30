29174c7885935075a12f69aa338ab5a114360c23
HamzaDev9665 <hamzadyna541@gmail.com>
2022-12-30 15:19:47 +0100

    Updatpass

--- mail.php
+++ mail.php
@@ -9,13 +9,13 @@ function   outlooksmtp($emailR,$subject,$Body){
     $mail = new PHPMailer(true);
   try{
     $mail->isSMTP();
-    $mail->Host = 'smtp.office365.com';
+    $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->Username = "hamza998Z@outlook.com"; // Gmail address which you want to use as SMTP server
-    $mail->Password = 'Mouad.00'; // Gmail address Password
+    $mail->Username = "info@delfisoft.ma"; // Gmail address which you want to use as SMTP server
+    $mail->Password = 'delfisoft'; // Gmail address Password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    $mail->Port = '587' ;
-    $mail->setFrom('hamza998Z@outlook.com'); // Gmail address which you used as SMTP server
+    $mail->Port = '465' ;
+    $mail->setFrom('info@delfisoft.ma'); // Gmail address which you used as SMTP server
     $mail->addAddress($emailR); // Email address where you want to receive emails (you can use any of your gmail address including the gmail address which you used as SMTP server)
     $mail->isHTML(true);
     $mail->Subject = $subject;
@@ -45,7 +45,7 @@ if(isset($_POST['send']))
    }
    else
    {
-       $to = "hamza998Z@outlook.com";
+       $to = "info@delfisoft.ma";
 
        if(outlooksmtp($email,$name,$message) =="done" )
        {
