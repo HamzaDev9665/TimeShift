7d3ffb31278c3f893c9be6985a18e607365ea221
HamzaDev9665 <hamzadyna541@gmail.com>
2022-12-30 15:06:47 +0100

    Update php

--- mail.php
+++ mail.php
@@ -1,15 +1,28 @@
-<?php
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $tel = $_POST['tel'];
-    $message = $_POST['message'];
-    $to = "hamzaloza@mailna.co";
+<?php 
 
-    $headers = "From: ".$name."\r\n"."CC:hamzaloza@mailna.co";
-    $txt ="You have reciverd an e-mail from".$name."\r\n"."Email: ".$email."Message: ".$message;
+if(isset($_POST['send']))
+{
+   $name = $_POST['name'];
+   $email = $_POST['email'];
+   $tel = $_POST['tel'];
+   $message = $_POST['message'];
 
-    if($email != NULL){
-        mail($to,$name,$message,$email);
-    }
+   if(empty($name) || empty($email) || empty($tel) || empty($message))
+   {
+       header('location:index.php?error');
+   }
+   else
+   {
+       $to = "hamzadyna541@gmail.com";
 
+       if(mail($to,$name,$message,$email))
+       {
+           header("location:index.php?success");
+       }
+   }
+}
+else
+{
+    header("location:index.php");
+}
 ?>
